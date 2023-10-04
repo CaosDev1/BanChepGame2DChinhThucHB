24ba1d95db90bb647495d3fe5418b89adc4f5c29
CaosDev1 <vhoang0327@gmail.com>
2023-10-04 20:41:49 +0700

    Fix Menu Tren Lop
    Chuyen menu bang code

--- Assets/_Game/Scripts/MainMenuUI.cs
+++ Assets/_Game/Scripts/MainMenuUI.cs
@@ -2,12 +2,53 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 using UnityEngine.VFX;
 
 public class MainMenuUI : MonoBehaviour
 {
-    public void PlayGame()
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject settingUI;
+    [SerializeField] private GameObject welcomeUI;
+    
+    [SerializeField] private Button playButton;
+    [SerializeField] private Button settingButton;
+    [SerializeField] private Button welcomeButton;
+    [SerializeField] private Button closeSettingButton;
+    [SerializeField] private Button closeWelcomeButton;
+
+    private void OnEnable()
+    {
+        playButton.onClick.AddListener(PlayGame);
+        settingButton.onClick.AddListener(SwitchSettingUI);
+        welcomeButton.onClick.AddListener(SwitchWelcomeUI);
+        closeSettingButton.onClick.AddListener(SettingClose);
+        closeWelcomeButton.onClick.AddListener(SettingClose);
+    }
+
+
+    private void PlayGame()
     {
         SceneManager.LoadSceneAsync(1);
+    }
+
+    private void SwitchSettingUI()
+    {
+        mainMenu.SetActive(false);
+        settingUI.SetActive(true);
+        
+    }
+
+    private void SwitchWelcomeUI()
+    {
+        mainMenu.SetActive(false);
+        welcomeUI.SetActive(true);
+    }
+
+    private void SettingClose()
+    {
+        mainMenu.SetActive(true);
+        settingUI.SetActive(false);
+        welcomeUI.SetActive(false);
     }
 }
